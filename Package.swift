2230f356e354478cf449d9eae8c7038332975bd7
Adam Bell <adam@adambell.me>
2020-09-15 22:08:48 -0700

    Add Swift Numerics as a Dependency

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/apple/swift-numerics", from: "0.0.5"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,6 +26,7 @@ let package = Package(
         .target(
             name: "Motion",
             dependencies: [
+                .product(name: "Numerics", package: "swift-numerics"),
             ]),
         .testTarget(
             name: "MotionTests",
