a0bb9e7701d2e5f4a76d90196e0e0103e709b52b
Adam Bell <adam@adambell.me>
2020-11-25 00:01:22 -0800

    Fix building docs

--- build_docs.sh
+++ build_docs.sh
@@ -2,5 +2,4 @@
 
 # Parse swift docs.
 
-jazzy --module Motion --swift-build-tool spm --build-tool-arguments -Xswiftc,-sdk,-Xswiftc,"`xcrun --sdk iphonesimulator --show-sdk-path`",-Xswiftc,-target,-Xswiftc,"x86_64-apple-ios13.0-simulator" --author "Adam Bell" --author_url "https://twitter.com/b3ll" --undocumented-text "No overview available." --theme apple --clean
-
+jazzy --module Motion --swift-build-tool spm --build-tool-arguments -Xswiftc,-swift-version,-Xswiftc,5 --author "Adam Bell" --author_url "https://twitter.com/b3ll" --undocumented-text "No overview available." --theme apple --clean
