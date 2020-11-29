67e83ec0d5ed6ae648cbf367d53f2d4e91a4b14a
Adam Bell <adam@adambell.me>
2020-11-29 00:10:20 -0800

    Migrate Benchmarking into Own Folder

--- Benchmark/Package.swift
+++ Benchmark/Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Motion",
+    name: "MotionBenchmark",
     platforms: [
         .iOS(.v13),
         .tvOS(.v13),
@@ -13,24 +13,28 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Motion",
-            targets: ["Motion"]),
+            name: "MotionBenchmark",
+            targets: ["MotionBenchmark"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-numerics", from: "0.0.8"),
+        .package(name: "Motion", path: "../"),
+        .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Motion",
+            name: "MotionBenchmark",
             dependencies: [
-                .product(name: "RealModule", package: "swift-numerics"),
+                "Motion",
+                "Benchmark",
             ]),
-        .testTarget(
-            name: "MotionTests",
-            dependencies: ["Motion"]),
+        .target(
+            name: "MotionBenchmarkRunner",
+            dependencies: [
+                "MotionBenchmark",
+            ]),
     ],
     swiftLanguageVersions: [.v5]
 )
