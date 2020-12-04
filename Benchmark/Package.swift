0829b72b7eb5885cfc8874407df017e5c47ad622
Adam Bell <adam@adambell.me>
2020-12-03 22:57:41 -0800

    Fix Benchmark Build

--- Benchmark/Package.swift
+++ Benchmark/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "Motion", path: "../"),
-        .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", .branch("master")),
+        .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
