5a0b34d437d4e549e54dd49c200e32c9e7c09f8c
Adam Bell <adam@adambell.me>
2020-12-09 18:15:07 -0800

    Remove Faulty SIMD RubberBanding Code and Fix RubberBanding

--- Sources/Motion/Utilities/RubberBanding.swift
+++ Sources/Motion/Utilities/RubberBanding.swift
@@ -20,10 +20,31 @@ public let UIScrollViewRubberBandingConstant = 0.55
    - range: The range over which the value won't rubberband.
  */
 public func rubberband<Value: FloatingPointInitializable>(_ value: Value, coefficient: Value = Value(UIScrollViewRubberBandingConstant), range: Value) -> Value {
-    // Without this, the swift type checker is super slow.
-    let x1 = (value * coefficient / range) + 1.0
 
-    return (1.0 - (1.0 / x1)) * range
+    if value <= range && value >= 0.0 {
+        return value
+    }
+
+    let x: Value
+    if value > range {
+        x = value - range
+    } else {
+        x = -value
+    }
+
+    // (1.0 - (1.0 / ((x * c / d) + 1.0))) * d
+
+    // Without this, the swift type checker is super slow.
+    let x1 = (x * coefficient / range) + 1.0
+
+    let rubberBandedAmount = ((1.0 - (1.0 / x1)) * range)
+
+    // We're beyond the range
+    if value > range {
+        return range + rubberBandedAmount
+    } else { // We're beyond the range in the opposite direction
+        return -rubberBandedAmount
+    }
 }
 
 /**
@@ -42,40 +63,4 @@ public func rubberband<Value: FloatingPointInitializable>(_ value: Value, coeffi
     return rubberband(value, coefficient: coefficient, range: range.upperBound - range.lowerBound)
 }
 
-/**
- Rubberbands a `Value` based on a given coefficient and range.
-
- - Parameters:
-   - value: The floating point value to rubberband.
-   - coefficient: A multiplier to decay the value when it's being rubberbanded. Defaults to `UIScrollViewRubberBandingConstant`.
-   - range: The range over which the value won't rubberband.
- */
-public func rubberband<Value: SIMDRepresentable>(_ value: Value, coefficient: Value.SIMDType.Scalar = Value.SIMDType.Scalar(UIScrollViewRubberBandingConstant), range: Value) -> Value {
-    typealias SIMDType = Value.SIMDType
-
-    // Same here.
-    let v = value.simdRepresentation()
-    let r = range.simdRepresentation()
-
-    let x1 = (v * coefficient / range.simdRepresentation()) + 1.0
-
-    let rubberbanded = (1.0 - (1.0 / x1)) * r
-
-    return Value(rubberbanded)
-}
-
-/**
- Rubberbands a `Value` based on a given coefficient and range.
-
- - Parameters:
-   - value: The floating point value to rubberband.
-   - coefficient: A multiplier to decay the value when it's being rubberbanded. Defaults to `UIScrollViewRubberBandingConstant`.
-   - range: The range over which the value won't rubberband.
- */
-public func rubberband<Value: SIMDRepresentable>(_ value: Value, coefficient: Value.SIMDType.Scalar = Value.SIMDType.Scalar(UIScrollViewRubberBandingConstant), range: ClosedRange<Value>) -> Value {
-    if range.contains(value) {
-        return value
-    }
-
-    return rubberband(value, coefficient: coefficient, range: range)
-}
+// TODO: SIMD variants at some point. It's kinda tricky.
