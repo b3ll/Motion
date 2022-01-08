80dde1d1918c07974f40d4f63de48a2fd75cae09
Jesse Grosjean <jesse@hogbaysoftware.com>
2022-01-08 13:10:36 -0500

    Maintain isPaused state from didSet instead of get/set, makes logic easier to reason about I think

--- Sources/Motion/Utilities/AnimationDriver.swift
+++ Sources/Motion/Utilities/AnimationDriver.swift
@@ -34,12 +34,14 @@ final class CoreAnimationDriver: AnimationDriver {
         displayLink.invalidate()
     }
 
-    var isPaused: Bool {
-        get {
-            displayLink.isPaused
-        }
-        set {
-            displayLink.isPaused = newValue
+
+    var isPaused: Bool = true {
+        didSet {
+            guard oldValue != isPaused else {
+                return
+            }
+            
+            displayLink.isPaused = isPaused
         }
     }
     
@@ -74,7 +76,7 @@ typealias SystemAnimationDriver = CoreVideoDriver
 final class CoreVideoDriver: AnimationDriver {
 
     private var displaylink: CVDisplayLink!
-    private var nextFrame: Synchronized<AnimationFrame?> = Synchronized(data: nil)
+    private var nextFrame: Synchronized<AnimationFrame?> = .init(data: nil)
 
     init?() {
         var displayLinkRef: CVDisplayLink? = nil
@@ -121,19 +123,17 @@ final class CoreVideoDriver: AnimationDriver {
     
     var observer: AnimationDriverObserver?
 
-    var isPaused: Bool {
-        get {
-            !CVDisplayLinkIsRunning(displaylink)
-        }
-        
-        set {
-            if newValue != isPaused {
-                if !newValue {
-                    CVDisplayLinkStart(displaylink)
-                } else {
-                    CVDisplayLinkStop(displaylink)
-                    nextFrame.value = nil
-                }
+    var isPaused: Bool = false {
+        didSet {
+            guard oldValue != isPaused else {
+                return
+            }
+            
+            if isPaused {
+                CVDisplayLinkStop(displaylink)
+            } else {
+                nextFrame.value = nil
+                CVDisplayLinkStart(displaylink)
             }
         }
     }
